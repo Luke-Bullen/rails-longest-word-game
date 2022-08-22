c119659f7af5c072df60252ad16c00d3e737dc85
Luke Bullen <lukeabullen@gmail.com>
2022-08-23 00:53:40 +0100

    done - testing not fully complete

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,17 +1,37 @@
 require "open-uri"
 
 class GamesController < ApplicationController
+
+
   def new
+    vowel = ["A", "E", "I", "O", "U"]
     @letters = []
-    10.times do
+    7.times do
       letter = ('A'..'Z').to_a
       @letters << letter.sample
+    end
+    3.times do
+      @letters << vowel.sample
     end
     @letters
   end
 
   def score
-    # raise
-    @url = "https://wagon-dictionary.herokuapp.com/#{params[:word]}"
+    @letters = params[:letters].split
+    @input = (params[:input] || "").upcase
+    @included = included?(@input, @letters)
+    @english_word = english_word?(@input)
+  end
+
+  private
+
+  def included?(word, letters)
+    word.chars.all? { |letter| word.count(letter) <= letters.count(letter) }
+  end
+
+  def english_word?(word)
+    response = URI.open("https://wagon-dictionary.herokuapp.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 end
